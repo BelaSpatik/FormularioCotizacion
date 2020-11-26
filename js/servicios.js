b99e6c5d7b232c106dacfa27f7e14d6a71aacb21
Bela Spatik <bela.spatik@gmail.com>
2020-11-26 16:35:18 -0300

    Creación del repositorio y subida de los archivos

---
+++ js/servicios.js
@@ -0,0 +1,83 @@
+var rubrosArray = [
+    {
+      "nombre": "Albañilería",
+      "obraNueva": true,
+      "reparacion": true,
+      "servicios": [["Construcción"], ["Revoque", "Terminaciones", "Mampostería", "Otros"]],
+      "sector": false,
+      "tipo": false,
+    },
+    {
+      "nombre": "Electricidad",
+      "obraNueva": true,
+      "reparacion": true,
+      "servicios": [["Instalaciones", "Tableros", "Habilitaciones"], ["Instalaciones", "Luminarias internas", "Luminarias externas", "Habilitaciones"]],
+      "sector": false,
+      "tipo": false
+    },
+    {
+      "nombre": "Gas",
+      "obraNueva": true,
+      "reparacion": true,
+      "servicios": [["Instalaciones"], ["Fugas", "Reconexiones", "Reparaciones", "Habilitaciones"]],
+      "sector": ["Cocina", "Baño", "Cloacas", "Desagües", "Piletas"],
+      "tipo": false
+    },
+    {
+      "nombre": "Pintura",
+      "obraNueva": true,
+      "reparacion": false,
+      "servicios": ["Aplicación", "Revestimiento", "Impermeabilización"],
+      "sector": false,
+      "tipo": false,
+    },
+    {
+      "nombre": "Pisos",
+      "obraNueva": true,
+      "reparacion": false,
+      "servicios": ["Instalaciones", "Recambios"],
+      "sector": false,
+      "tipo": ["Piso flotante", "Piso alfombrado", "Piso de cerámica"]
+    },
+    {
+      "nombre": "Placas",
+      "obraNueva": true,
+      "reparacion": false,
+      "servicios": ["Colocación"],
+      "sector": false,
+      "tipo": ["Ambientación", "Paredes", "Cielorraso"]
+    },
+    {
+      "nombre": "Plomería",
+      "obraNueva": true,
+      "reparacion": true,
+      "servicios": [["Instalaciones"], ["Pérdidas", "Reconexiones", "Reparaciones"]],
+      "sector": false,
+      "tipo": false
+    },
+    {
+      "nombre": "Techos",
+      "obraNueva": true,
+      "reparacion": true,
+      "servicios": [["Instalación"], ["Reparaciones"]],
+      "sector": false,
+      "tipo": ["Pérgolas", "Galerías", "Aleros", "Otros"]
+    },
+    {
+      "nombre": "Zinguería",
+      "obraNueva": true,
+      "reparacion": true,
+      "servicios": [["Instalaciones"], ["Reparaciones", "Limpieza"]],
+      "sector": false,
+      "tipo": ["Desagües", "Canaletas", "Bajadas"]
+    },
+    {
+      "nombre": "Otros",
+      "obraNueva": true,
+      "reparacion": true,
+      "servicios": false,
+      "sector": false,
+      "tipo": false,
+      }
+  ];
+  
